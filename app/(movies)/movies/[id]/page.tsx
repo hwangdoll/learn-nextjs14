501d1d037e391607bb7db8cc4f638ec539e3e51d
hwangdoll <dolljjin@gmail.com>
2025-05-21 12:54:21 +0900

    edit3

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,11 +2,13 @@ import { Suspense } from "react";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+interface MobieDetailParam {
+  params: {
+    id: string;
+  };
+}
+
+export default async function MovieDetail({ params: { id } }) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info...</h1>}>
