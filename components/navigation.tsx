d3fd520e166c9885079b4a61bc96134e8aafb5c2
hwangdoll <dolljjin@gmail.com>
2025-05-21 15:39:11 +0900

    edit8

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,11 +1,13 @@
 "use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.modules.css";
 
 export default function Navigation() {
   const path = usePathname();
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link> {path === "/" ? "😀" : ""}
